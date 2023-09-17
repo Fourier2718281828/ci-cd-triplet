8bc4da9fa435e42448821850e5ed1f72216a90b3
Fourier2718281828 <non-linear@ukr.net>
2023-09-17 17:32:12 +0300

    added and tested: StringAlgorithms

--- src/main/java/org/example/StringAlgorithms.java
+++ src/main/java/org/example/StringAlgorithms.java
@@ -1,2 +1,14 @@
-package org.example;public class StringAlgorithms {
+package org.example;
+
+public class StringAlgorithms {
+    public static String revertString(String str) {
+        char[] chars = str.toCharArray();
+        for(var i = 0; i < chars.length / 2; ++i) {
+            var buff = chars[i];
+            chars[i] = chars[chars.length - i - 1];
+            chars[chars.length - i - 1] = buff;
+        }
+
+        return new String(chars);
+    }
 }
--- src/test/java/org/example/StringAlgorithmsTest.java
+++ src/test/java/org/example/StringAlgorithmsTest.java
@@ -1,4 +1,29 @@
-import junit.framework.TestCase;
-public class StringAlgorithmsTest extends TestCase {
-  
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringAlgorithmsTest {
+
+    @Test
+    public void revertStringTest() {
+        var strings = new String[] {
+                "abc",
+                "bcdasdasgdsaf",
+                "spysdasdojgasp"
+        };
+        var revertedStrings = new String[strings.length];
+
+        for(var i = 0; i < strings.length; ++i) {
+            var reversedStringBuilder = new StringBuilder(strings[i]);
+            revertedStrings[i] = reversedStringBuilder.reverse().toString();
+        }
+
+        for(var i = 0; i < strings.length; ++i) {
+            var customReverted = StringAlgorithms.revertString(strings[i]);
+            assertEquals(customReverted, revertedStrings[i]);
+        }
+    }
+
 }
